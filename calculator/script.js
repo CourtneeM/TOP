bf7c0b6e505061f78c1c34df1fae2fbce85336db
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-04-29 06:58:44 -0500

    Update display when performing calculations

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,7 @@
+const display = document.querySelector('#calculator-display');
+const buttons = document.querySelectorAll('.btn');
+let expression = [];
+
 function add (a, b) {
   return a + b;
 }
@@ -18,7 +22,57 @@ function operate(operator, a, b) {
   switch (operator) {
     case '+': return add(a, b);
     case '-': return subtract(a, b);
-    case '*': return multiply(a, b);
+    case 'x': return multiply(a, b);
     case '/': return divide(a, b);
   }
 }
+
+function calculate(expression) {
+  let [firstOperand, operator, secondOperand] = expression.join('').split(' ');
+  return String(operate(operator, Number(firstOperand), Number(secondOperand))).split('');
+}
+
+function storeValues(e) {
+  let btnValue = e.target.textContent;
+
+  if (Number(btnValue) || btnValue === '0' || btnValue === '.') {
+    expression.push(btnValue);
+  }
+
+  if (expression.length > 0) {
+    switch (btnValue) {
+      case '+':
+      case '-':
+      case 'x':
+      case '/':
+        operator = btnValue;
+        expression.push(' ', btnValue, ' ');
+        break;
+      case 'DEL':
+        expression.pop();
+        break;
+      case 'C':
+        expression = [];
+        break;
+    }
+
+    if (btnValue === '=') {
+      expression = calculate(expression);
+    }
+
+    if (expression.length > 0) { 
+      display.textContent = expression.join('');
+    } else {
+      display.textContent = '0';
+    }
+  }
+
+  console.log(expression);
+
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', e => {
+    storeValues(e);
+  });
+});
