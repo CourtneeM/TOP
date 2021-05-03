9728f02f44da7c4c5d9888ede25caf172af9086a
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-05-03 14:24:41 -0500

    Add win conditions and check for win or tie

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -33,12 +33,12 @@ const displayController = (() => {
   return { generateBoard, clearBoard };
 })();
 
+const createPlayer = (name, marker) => ({ name, marker });
+
+const player1 = createPlayer('mark', 'X');
+const player2 = createPlayer('sally', 'O');
 
 const playGame = (() => {  
-  const createPlayer = (name, marker) => ({ name, marker });
-
-  const player1 = createPlayer('mark', 'X');
-  const player2 = createPlayer('sally', 'O');
   let marker = player1.marker;
 
   const placeMarker = (index)=> {
@@ -47,10 +47,40 @@ const playGame = (() => {
     displayController.clearBoard();
     displayController.generateBoard();
     marker === player1.marker ? marker = player2.marker : marker = player1.marker;
+    checkForWin();
   }
+
+  const endGame = player => {
+    alert(`${player.name} is the winner!`);
+    document.querySelector('#game-board-container').style['pointer-events'] = 'none';
+  }
+
+  const checkForWin = () => {
+    const board = gameBoard.board;
+    const winConditions = [[...board.slice(0, 3)], [...board.slice(3, 6)], [...board.slice(6, 9)],
+                           [board[0], board[3], board[6]], [board[1], board[4], board[7]], [board[2], board[5], board[8]],
+                           [board[0], board[4], board[8]], [board[2], board[4], board[6]]];
+    
+    winConditions.filter(condition => {
+      if (condition.every(marker => marker === player1.marker)) {
+        return endGame(player1);
+      } else if (condition.every(marker => marker === player2.marker)) {
+        return endGame(player2);
+      }
+    });
+
+    if (board.every(square => square)) {
+      alert("It's a tie!");
+    }
+
+    
+  }
+  
 
   displayController.generateBoard();
 
   return { placeMarker };
   
 })();
+
+// check for win, win conditions
