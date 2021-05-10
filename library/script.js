367f8b156696efcab9f1fb726206e52b8df2fce7
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-05-10 14:13:12 -0500

    Change Book constructor to a class

--- library/script.js
+++ library/script.js
@@ -5,11 +5,13 @@ let bookshelfTable = document.createElement('table');
 
 let myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus;
+class Book {
+  constructor(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
 }
 
 function addBookToLibrary() {
