917e7fb7f2859389fa227eca49d34cd865ddcf8d
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-05-12 18:36:15 -0500

    Fix errors in loading home page

--- restaurant_page/src/index.js
+++ restaurant_page/src/index.js
@@ -1,16 +1,24 @@
+import header from './modules/display_controllers/header'; 
 import home from './modules/display_controllers/home';
 
 const contentContainer = document.querySelector('#content');
 
+while (contentContainer.firstChild) {
+  contentContainer.removeChild(contentContainer.firstChild);
+}
+
+contentContainer.appendChild(header);
+
+
 let activePage = document.querySelector('.active');
-// switch (activePage.textContent) {
-//   case 'Home':
-//     contentContainer.appendChild(home);
-//     break;
+switch (activePage.textContent) {
+  case 'Home':
+    contentContainer.appendChild(home);
+    break;
   // case 'Menu':
   //   contentContainer.appendChild(menu);
   //   break;
   // case 'Contact':
   //   contentContainer.appendChild(contact);
   //   break;
-// }
+}
--- restaurant_page/src/modules/display_controllers/home.js
+++ restaurant_page/src/modules/display_controllers/home.js
@@ -1,12 +1,12 @@
-import header from './header'; 
 import hero from './home_components/hero';
 import about from './home_components/about';
 
 const home = () => {
-  const contentContainer = document.querySelector('#content');
-  contentContainer.appendChild(header);
-  contentContainer.appendChild(hero);
-  contentContainer.appendChild(about);
+  const main = document.createElement('main');
+  main.appendChild(hero);
+  main.appendChild(about);
+
+  return main;
 };
 
 export default home();
