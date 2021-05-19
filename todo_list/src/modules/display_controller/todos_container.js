5071999f6921c955e6128995f2a66c46a2a995b3
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-05-18 20:12:43 -0500

    Add working edit button and edit confirmation button will update todos array with new values

--- todo_list/src/modules/display_controller/todos_container.js
+++ todo_list/src/modules/display_controller/todos_container.js
@@ -157,11 +157,13 @@ const todosContainer = (() => {
 
   const todosEventHandlers = function(todos, Todo) {
     const todoContainers = [...todosListContainer.children];
+    const todoInfoContainers = [...document.querySelectorAll('.todo-info-container')];
     const removeTodoCheckboxes = [...document.querySelectorAll('.remove-todo-checkbox')];
     const todosControlsContainer = document.querySelector('#todos-controls-container');
     const todosControlsBtnsContainer = document.querySelector('#todos-control-btns-container');
     const todosControlBtns = [...todosControlsBtnsContainer.children];
     const todosControlActionBtns = [...document.querySelectorAll('.btn-todos-control-action')];
+    const fieldNames = Object.keys(todos.list[0].todos[0]);
 
     // Todo Controls Buttons 
     todosControlBtns.forEach(control => {
@@ -174,9 +176,29 @@ const todosContainer = (() => {
             break;
           case 'Edit':
             document.querySelector('#edit-todo-container').style.display = 'flex';
+            todoInfoContainers.forEach(todoInfoContainer => {
+              todoInfoContainer.removeAttribute('class');
+              todoInfoContainer.classList.add('edit-todo-info-container');
+
+              [...todoInfoContainer.children].forEach((field, index) => {
+                const label = document.createElement('label');
+                const input = document.createElement('input');
+                
+                label.textContent = ((fieldNames[index])[0].toUpperCase() + fieldNames[index].slice(1));
+                input.setAttribute('type', 'text');
+                input.classList.add('edit-todo-field-input');
+                input.value = field.textContent;
+
+                label.appendChild(input);
+                todoInfoContainer.replaceChild(label, field);
+              });
+            });
+            // priority should be a drop down, 1-5
             break;
           case 'Add':
             document.querySelector('#add-todo-container').style.display = 'flex';
+            // create a blank todo container with the inputs in the place of text spots
+            // priority should be a drop down, 1-5
             break;
         }
       });
@@ -184,6 +206,7 @@ const todosContainer = (() => {
 
     todosControlActionBtns.forEach(actionBtn => {
       const selectedProjectName = document.querySelector('#selected-project-name').textContent;
+      const selectedProject = todos.list.filter(project => project.name === selectedProjectName)[0];
       const removeTodoContainer = document.querySelector('#remove-todo-container');
       const editTodoContainer = document.querySelector('#edit-todo-container');
       const addTodoContainer = document.querySelector('#add-todo-container');
@@ -198,23 +221,36 @@ const todosContainer = (() => {
               }
             }).filter(index => index || index === 0).reverse();
 
-            todos.list.forEach(project => {
-              if (project.name === selectedProjectName) {
-                removeIndices.forEach(index => project.deleteTodo(index));
-              }
-            });
+            removeIndices.forEach(index => selectedProject.deleteTodo(index));
           }
         }
         
         if (editTodoContainer.style.display === 'flex') {
           if (actionBtn.textContent === 'Edit') {
-            
+            const newValues = [...document.querySelectorAll('.edit-todo-info-container')].map(todoContainer => {
+              return [...todoContainer.children].map(label => label.querySelector('input').value);
+            });
+
+            selectedProject.todos.forEach((todo, todoIndex) => {
+              fieldNames.forEach((field, fieldIndex) => {
+                todo[field] = newValues[todoIndex][fieldIndex];
+              });
+            });
           }
+
+          [...todoInfoContainers].forEach(todoInfoContainer => {
+            todoInfoContainer.removeAttribute('class');
+            todoInfoContainer.classList.add('todo-info-container');
+          });
         }
         
         if (addTodoContainer.style.display === 'flex') {
           if (actionBtn.textContent === 'Add') {
-            
+            // todos.list.forEach(project => {
+            //   if (project.name === selectedProjectName) {
+            //     project.addTodo(new Todo(...newValues));
+            //   }
+            // });
           }
         }
 
