708e123c86af06430c314f66c33d4dbcaf6768dc
Courtnee M <courtneemeaghan@gmail.com>
2021-05-24 08:47:54 -0500

    Refactor main_container code

--- todo_list/src/modules/display_controller/main_container.js
+++ todo_list/src/modules/display_controller/main_container.js
@@ -5,30 +5,27 @@ import '../../styles/main/main.css';
 const mainContainer = (todos, Project, Todo) => {
   const todosList = todos.list
   const contentContainer = document.querySelector('#content-container');
-  contentContainer.appendChild(projectsContainer.projectsHandler(todos));
 
+  contentContainer.appendChild(projectsContainer.projectsHandler(todos));
   contentContainer.appendChild(todosContainer.todosHandler(todosList));
 
   projectsContainer.projectsEventHandlers(todos, Project);
   todosContainer.todosEventHandlers(todosList, Todo);
-  let observerSelectProject = new MutationObserver(function() {
+
+  new MutationObserver(function observerSelectProject() {
     projectsContainer.updateHeaderTitle();
     todosContainer.clearTodos();
     todosContainer.displayTodos(todosList);
     todosContainer.clearControlsContainer();
     todosContainer.todosControlsContainers.todosControlsHandler()
     todosContainer.todosEventHandlers(todosList, Todo);
-  });
-
-  observerSelectProject.observe(document.querySelector('#projects-list-container'),
-                                          { subtree: true, childList: true, attributes: true, attributeFilter: ['id'] });
-                                  
-  let observerUpdateProjectsTodoCount = new MutationObserver(function() {
+  }).observe(document.querySelector('#projects-list-container'),
+             { subtree: true, childList: true, attributes: true, attributeFilter: ['id'] });
+                                
+  new MutationObserver(function observerUpdateProjectsTodoCount() {
     projectsContainer.updateProjectTodoCount(todos);
-  });
-
-  observerUpdateProjectsTodoCount.observe(document.querySelector('#todos-list-container'),
-                                          { childList: true });
+  }).observe(document.querySelector('#todos-list-container'),
+             { childList: true });
 };
 
 export default mainContainer;
