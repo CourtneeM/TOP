553e4d1b333b578a016a97e68de0e61d62c4ede5
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-06-08 14:29:07 -0500

    Add automatic slideshow interval

--- dynamic_ui_practice/image_slider/script.js
+++ dynamic_ui_practice/image_slider/script.js
@@ -4,22 +4,32 @@ const slides = [...document.querySelectorAll('.slide')];
 const progressCircles = [...document.querySelectorAll('.progress-circle')];
 let currentSlideIndex = 0;
 
-prevSlideBtn.addEventListener('click', () => {
+function previousSlide() { 
   slides[currentSlideIndex].removeAttribute('id');
   progressCircles[currentSlideIndex].removeAttribute('id');
-
+  
   currentSlideIndex > 0 ? currentSlideIndex -= 1 : currentSlideIndex = slides.length - 1;
-
+  
   slides[currentSlideIndex].id = 'current-slide';
   progressCircles[currentSlideIndex].id = 'current-slide-circle';
+}
+
+function nextSlide() {
+  slides[currentSlideIndex].removeAttribute('id');
+  progressCircles[currentSlideIndex].removeAttribute('id');
+  
+  currentSlideIndex < slides.length - 1 ? currentSlideIndex += 1 : currentSlideIndex = 0;
+  
+  slides[currentSlideIndex].id = 'current-slide';
+  progressCircles[currentSlideIndex].id = 'current-slide-circle';
+}
+
+prevSlideBtn.addEventListener('click', () => {
+  previousSlide();    
 });
 
 nextSlideBtn.addEventListener('click', () => {
-  slides[currentSlideIndex].removeAttribute('id');
-  progressCircles[currentSlideIndex].removeAttribute('id');
-
-  currentSlideIndex < slides.length - 1 ? currentSlideIndex += 1 : currentSlideIndex = 0;
-
-  slides[currentSlideIndex].id = 'current-slide';
-  progressCircles[currentSlideIndex].id = 'current-slide-circle';
+  nextSlide();
 });
+
+setInterval(() => nextSlide(), 5000);
