c5416e7a96b35d7fa98e411934fdd39e40100a0b
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-06-15 14:32:58 -0500

    Extract data from weather object and allow for city input

--- weather_app/src/index.js
+++ weather_app/src/index.js
@@ -1,17 +1,54 @@
 import API_KEY from './keys.js';
-let currentTemp;
-let cityName = 'Beaumont';
+const cityInput = document.querySelector('#city-name-input');
+const searchBtn = document.querySelector('button');
+let cityName = 'Branson'
 
-
-async function getWeatherData() {
-  try {
-    const plainWeatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`);
-    const weatherData = await plainWeatherData.json();
-    currentTemp = weatherData.main.temp;
-    console.log(`Current Temperature: ${currentTemp}\xB0F`);
-  } catch (error) {
-    return 'Something went wrong: ' + error;
+const render = () => {
+  async function getWeatherData() {
+    try {
+      const plainWeatherData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`);
+      const weatherData = await plainWeatherData.json();
+      return weatherData;
+    } catch (error) {
+      return 'Something went wrong: ' + error;
+    }
   }
+
+  const specificWeatherData = (async () => {
+    try {
+      const weatherData = await getWeatherData();
+      console.log(weatherData);
+      
+      const currentTemp = `The current temperature is: ${weatherData.main.temp}\xB0F`;
+      const highTemp = `The high today is: ${weatherData.main.temp_max}\xB0F`;
+      const lowTemp = `The low today is: ${weatherData.main.temp_min}\xB0F`;
+      const feelsLike = weatherData.main.feels_like;
+      const weatherConditions = weatherData.weather.map(condition => condition.main);
+
+      return {
+        currentTemp,
+        highTemp,
+        lowTemp,
+        feelsLike,
+        weatherConditions,
+      }
+    } catch (error) {
+      console.log('Something went wrong: ' + error);
+    }
+  })();
+
+  specificWeatherData.then(res => {
+    const { currentTemp, highTemp, lowTemp, feelsLike, weatherConditions } = res;
+    console.log(weatherConditions);
+  }).catch(error => {
+    console.log('Something went wrong: ' + error);
+  });
 }
 
-getWeatherData();
+render();
+
+searchBtn.addEventListener('click', () => {
+  cityName = cityInput.value;
+  render();
+  cityInput.value = '';
+});
