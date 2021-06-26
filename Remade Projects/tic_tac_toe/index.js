107e3a7a99c513ae4da8ca1c82699eb473ad8a04
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-06-26 16:15:44 -0500

    Add modules for displayController and gameboard display

--- Remade Projects/tic_tac_toe/index.js
+++ Remade Projects/tic_tac_toe/index.js
@@ -21,19 +21,67 @@ const gameboardController = (() => {
 })();
 
 const displayController = (() => {
-  const generateGameboard = () => {
-
-  }
-
-  const updateGameboard = (index, marker) => {
-
-  }
-
-  const clearGameboard = () => {
-
-  }
+  const body = document.querySelector('body');
   
-  return { generateGameboard, updateGameboard, clearGameboard }
+  const generatePreGameElements = () => {
+    const preGameContainer = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const player1NameInput = document.createElement('input');
+    const player2NameInput = document.createElement('input');
+    const startGameBtn = document.createElement('button');
+
+    preGameContainer.id = 'pre-game-container';
+    player1NameInput.id = 'player1-name-input';
+    player2NameInput.id = 'player2-name-input';
+    startGameBtn.id = 'start-game-btn';
+
+    h1.textContent = 'Tic-Tac-Toe';
+    player1NameInput.value = 'Player 1';
+    player2NameInput.value = 'Computer';
+    startGameBtn.textContent = 'Start Game';
+
+    [h1, player1NameInput, player2NameInput, startGameBtn].forEach(el => preGameContainer.appendChild(el));
+    body.appendChild(preGameContainer);
+  }
+
+  const generatePostGameElements = () => {
+    
+  }
+
+  const gameboard = (() => {
+    const generate = () => {
+      const gameboardSquares = 9;
+      const gameboardContainer = document.createElement('div');
+      gameboardContainer.id = 'gameboard-container';
+  
+      for (let i = 0; i < gameboardSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        gameboardContainer.appendChild(square);
+      }
+  
+      body.appendChild(gameboardContainer);
+    }
+
+    const update = (index, marker) => {
+      const squares = [...document.querySelectorAll('.square')];
+      squares[index].textContent = marker;
+    }
+  
+    const clear = () => {
+      const squares = [...document.querySelectorAll('.square')];
+      squares.forEach(square => square.textContent = '');
+    }
+
+    return { generate, update, clear }
+  })();
+
+  (function initialRender() {
+    generatePreGameElements();
+    gameboard.generate();
+  })();
+
+  return { gameboard }
 })();
 
 const eventHandlers = (() => {
