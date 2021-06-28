c48fe5ba828388d6fb0ee3e8588cdf20f4eacce4
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-06-28 10:39:53 -0500

    Add number players selection

--- Remade Projects/tic_tac_toe/index.js
+++ Remade Projects/tic_tac_toe/index.js
@@ -1,5 +1,8 @@
-const createPlayer = (name, marker) => ({ name, marker });
+const createPlayer = (name, marker) => { 
+  return { name, marker }
+};
 const players = {};
+let numPlayers;
 
 const gameboardController = (() => {
   let gameboard = [];
@@ -18,6 +21,8 @@ const gameboardController = (() => {
     }
   }
 
+  const checkForEmptySquare = index => gameboardController.gameboard[index] === '';
+
   const checkForWinner = marker => {
     const winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
     const currentGameboard = winningMoves.map(move => [gameboard[move[0]], gameboard[move[1]], gameboard[move[2]]])
@@ -29,46 +34,105 @@ const gameboardController = (() => {
     return winner;
   }
 
-  return { gameboard, generate, update, clear, checkForWinner }
+  return { gameboard, generate, update, clear, checkForEmptySquare, checkForWinner }
 })();
+
+const playerTurn = (index, marker) => {
+  gameboardController.update(index, marker);
+  displayController.gameboard.update(index, marker);
+}
+
+const computerTurn = () => {
+
+}
 
 const displayController = (() => {
   const body = document.querySelector('body');
 
   const preGameElements = (() => {
     const generate = () => {
+      const generateNumPlayersElements = () => {
+        const numPlayersContainer = document.createElement('div');
+        const onePlayerLabel = document.createElement('label');
+        const onePlayerRadio = document.createElement('input');
+        const twoPlayersLabel = document.createElement('label');
+        const twoPlayersRadio = document.createElement('input');
+
+        numPlayersContainer.id = 'num-players-container';
+        onePlayerRadio.id = 'one-player-radio';
+        twoPlayersRadio.id = 'two-players-radio';
+
+        onePlayerRadio.setAttribute('type', 'radio');
+        onePlayerRadio.setAttribute('name', 'num-players');
+        onePlayerRadio.setAttribute('value', '1 Player');
+        onePlayerRadio.setAttribute('checked', true);
+
+        twoPlayersRadio.setAttribute('type', 'radio');
+        twoPlayersRadio.setAttribute('name', 'num-players');
+        twoPlayersRadio.setAttribute('value', '2 Players');
+
+        onePlayerLabel.textContent = '1 Player';
+        twoPlayersLabel.textContent = '2 Players';
+
+        onePlayerLabel.appendChild(onePlayerRadio);
+        twoPlayersLabel.appendChild(twoPlayersRadio);
+        [onePlayerLabel, twoPlayersLabel].forEach(el => numPlayersContainer.appendChild(el));
+
+        return numPlayersContainer;
+      }
+
+      const generateInputElements = () => {
+        const inputsContainer = document.createElement('div');
+        const player1NameInput = document.createElement('input');
+        const player2NameInput = document.createElement('input');
+
+        inputsContainer.id = 'inputs-container';
+        player1NameInput.id = 'player1-name-input';
+        player2NameInput.id = 'player2-name-input';
+
+        player1NameInput.value = 'Player 1';
+        player2NameInput.value = 'Computer';
+
+        player2NameInput.disabled = true;
+
+        [player1NameInput, player2NameInput].forEach(el => inputsContainer.appendChild(el));
+
+        return inputsContainer;
+      }
+
       const preGameContainer = document.createElement('div');
       const h1 = document.createElement('h1');
-      const player1NameInput = document.createElement('input');
-      const player2NameInput = document.createElement('input');
+      const numPlayersContainer = generateNumPlayersElements();
+      const inputsContainer = generateInputElements();
       const startGameBtn = document.createElement('button');
   
       preGameContainer.id = 'pre-game-container';
-      player1NameInput.id = 'player1-name-input';
-      player2NameInput.id = 'player2-name-input';
       startGameBtn.id = 'start-game-btn';
-  
+      
       h1.textContent = 'Tic-Tac-Toe';
-      player1NameInput.value = 'Player 1';
-      player2NameInput.value = 'Computer';
       startGameBtn.textContent = 'Start Game';
-  
-      [h1, player1NameInput, player2NameInput, startGameBtn].forEach(el => preGameContainer.appendChild(el));
+
+      [h1, numPlayersContainer, inputsContainer, startGameBtn].forEach(el => preGameContainer.appendChild(el));
       body.appendChild(preGameContainer);
     }
 
     const remove = preGameContainer => {
-      const inputs = [...preGameContainer.querySelectorAll('input')];
+      const numPlayerRadios = [...document.querySelector('#num-players-container').querySelectorAll('input')];
+      const nameInputs = [...document.querySelector('#inputs-container').querySelectorAll('input')];
       const startGameBtn = preGameContainer.querySelector('#start-game-btn');
-  
-      inputs.forEach((input, index) => {
-        index === 0 ? players[input.value] = 'X' : players[input.value] = 'O';
-        preGameContainer.removeChild(input);
+
+      numPlayers = numPlayerRadios.filter(radio => radio.checked)[0].value;
+
+      nameInputs.forEach((input, index) => {
+        const marker = index === 0 ? 'X' : 'O';
+        const player = createPlayer(input.value, marker);
+        players[player.name] = player.marker;
       });
-  
+
+      preGameContainer.removeChild(document.querySelector('#num-players-container'));
+      preGameContainer.removeChild(document.querySelector('#inputs-container'));
       preGameContainer.removeChild(startGameBtn);
     }
-
     return { generate, remove }
   })();
 
@@ -200,6 +264,23 @@ const displayController = (() => {
 })();
 
 const eventHandlers = (() => {
+  const numPlayersListener = (() => {
+    const radioBtns = [...document.querySelector('#num-players-container').querySelectorAll('input')];
+    const nameInputs = [...document.querySelector('#inputs-container').querySelectorAll('input')];
+
+    radioBtns.forEach(radio => {
+      radio.addEventListener('click', () => {
+        if (radio.value === '1 Player') {
+          nameInputs[1].disabled =  true;
+          nameInputs[1].value = 'Computer';
+        } else {
+          nameInputs[1].disabled = false
+          nameInputs[1].value = 'Player 2';
+        }
+      });
+    });
+  })();
+
   const startGameListener = () =>{
     document.querySelector('#start-game-btn').addEventListener('click', () => {
       gameboardController.generate();
@@ -210,23 +291,32 @@ const eventHandlers = (() => {
   }
 
   const squaresListener = () => {
-    const checkForEmptySquare = index => gameboardController.gameboard[index] === '';
-
+    const winEvent =  marker => {
+      const winner = `${Object.keys(players)[Object.values(players).indexOf(marker)]}(${marker})`;
+      displayController.postGameRender(winner);
+      newGameListener();
+    }
+    
     [...document.querySelectorAll('.square')].forEach((square, index) => {
       square.addEventListener('click', () => {
-        const [player1, player2] = Object.keys(players);
-        const marker = gameboardController.gameboard.filter(square => square === '').length % 2 !== 0 ? players[player1] : players[player2];
+        const [player1, player2] = Object.keys(players);        
+        if (numPlayers === '2 Players') { 
+          const marker = gameboardController.gameboard.filter(square => square === '').length % 2 !== 0 ? players[player1] : players[player2];
+        
+          if (!gameboardController.checkForEmptySquare(index)) return;
+          playerTurn(index, marker);
+          displayController.roundNumberHandler.increment();
+          if (gameboardController.checkForWinner(marker)) return winEvent(marker);
+        }
 
-        if (!checkForEmptySquare(index)) return;
+        if (numPlayers === '1 Player') {
+          playerTurn(index, players[player1])
+          displayController.roundNumberHandler.increment();
+          if (gameboardController.checkForWinner(players[player1])) return winEvent(players[player1]);
 
-        gameboardController.update(index, marker);
-        displayController.gameboard.update(index, marker);
-        displayController.roundNumberHandler.increment();
-
-        if (gameboardController.checkForWinner(marker)) {
-          const winner = `${Object.keys(players)[Object.values(players).indexOf(marker)]}(${marker})`;
-          displayController.postGameRender(winner);
-          newGameListener();
+          computerTurn(players[player2]) // calculate index within function
+          displayController.roundNumberHandler.increment();
+          if (gameboardController.checkForWinner(players[player2])) return winEvent(players[player2]);
         }
       });
     });
