112965b09656baab4032d0ff42869d224baff0c6
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-07-29 12:02:40 -0500

    Allow user to edit each task

--- react-inputs-and-lists-practice/src/App.js
+++ react-inputs-and-lists-practice/src/App.js
@@ -9,12 +9,16 @@ class App extends Component {
     this.state = {
       task: {
         text: '',
-        id: uniqid()
+        id: uniqid(),
+        edit: false
       },
       tasks: [],
     }
 
+    this.handleEditTask = this.handleEditTask.bind(this);
     this.handleDeleteTask = this.handleDeleteTask.bind(this);
+    this.setEditTrue = this.setEditTrue.bind(this);
+    this.handleSubmitEditTask = this.handleSubmitEditTask.bind(this);
   }
 
   handleInputChange(e) {
@@ -22,23 +26,61 @@ class App extends Component {
       task: {
         text: e.target.value,
         id: this.state.task.id,
+        edit: false,
       }
     });
   }
 
   addTask() {
+    if (this.state.task.text === '') return;
+
     this.setState((prevState) => ({
       task: {
         text: '',
-        id: uniqid()
+        id: uniqid(),
+        edit: false,
       },
       tasks: [...prevState.tasks, this.state.task],
     }));
   }
 
+  handleEditTask(index) {
+    if (this.state.task.text === '') return;
+
+    let tasksCopy = [...this.state.tasks];
+    tasksCopy.splice(index, 1, this.state.task);
+    
+    this.setState({
+      task: {
+        text: '',
+        id: uniqid()
+      },
+      tasks: tasksCopy,
+    });
+  }
+
   handleDeleteTask(index) {
     const tasksCopy = [...this.state.tasks];
     tasksCopy.splice(index, 1);
+
+    this.setState({
+      tasks: tasksCopy,
+    });
+  }
+
+  setEditTrue(index) {
+    const tasksCopy = [...this.state.tasks];
+    tasksCopy[index].edit = true;
+
+    this.setState({
+      tasks: tasksCopy,
+    });
+  }
+
+  handleSubmitEditTask(editedTaskText, index) {
+    let tasksCopy = [...this.state.tasks];
+    tasksCopy[index].text = editedTaskText;
+    tasksCopy[index].edit = false;
 
     this.setState({
       tasks: tasksCopy,
@@ -58,7 +100,7 @@ class App extends Component {
           />
         </label>
 
-        <Overview tasks={this.state.tasks} deleteTask={this.handleDeleteTask}/>
+        <Overview tasks={this.state.tasks} editTask={this.handleEditTask} deleteTask={this.handleDeleteTask} setEditTrue={this.setEditTrue} handleSubmitEditTask={this.handleSubmitEditTask}/>
 
         <button onClick={() => this.addTask()}>Submit</button>
       </div>
--- react-inputs-and-lists-practice/src/components/Overview.js
+++ react-inputs-and-lists-practice/src/components/Overview.js
@@ -3,18 +3,47 @@ import React, { Component } from 'react';
 class Overview extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      editedTaskText: '',
+    }
+  }
+
+  displayTasks() {
+    return this.props.tasks.map((task, index) => {
+      if (task.edit) {
+        return this.displayEditField(task, index);
+      } else {
+        return this.displayTask(task, index);
+      }
+    })
+  }
+
+  displayTask(task, index) {
+    return <li key={task.id}>
+             {index + 1}: {task.text}
+             <button onClick={() => this.props.setEditTrue(index)}>Edit</button>
+             <button onClick={() => this.props.deleteTask(index)}>Delete</button>
+           </li>
+  }
+  
+  displayEditField(task, index) {
+    return <li key={task.id}>
+             <input type="text" placeholder={task.text} onChange={(e) => this.handleInputChange(e)}/>
+             <button onClick={() => this.props.handleSubmitEditTask(this.state.editedTaskText, index)}>Submit</button>
+           </li>
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      editedTaskText: e.target.value,
+    });
   }
 
   render() {
     return (
       <ul>
-        { this.props.tasks.map((task, index) => {
-            return <li key={task.id}>
-              {index + 1}: {task.text}
-              <button onClick={() => this.props.deleteTask(index)}>Delete</button>
-            </li>
-          })
-        }
+        { this.displayTasks() }
       </ul>
     );
   }
